64222bbe8ba84227877d6e7855e00cbc8975bb9e
Dan Visan <visan.dan.constantin@gmail.com>
2019-05-18 16:23:51 +0300

    Setup project

---
+++ src/main/java/ro/vis/transactionview/TransactionViewApplication.java
@@ -0,0 +1,13 @@
+package ro.vis.transactionview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionViewApplication.class, args);
+	}
+
+}
