ab6cea9332fd01a4a5144605893b3675063151a8
Dan Visan <visan.dan.constantin@gmail.com>
2019-05-19 11:33:00 +0300

    Prepare initial data

--- src/main/resources/db/migration/V1__transasctions_schema.sql
+++ src/main/resources/db/migration/V1__transasctions_schema.sql
@@ -1,5 +1,6 @@
-create table balances (
-    user_id      varchar(255) primary key,
+create table balances
+(
+    client_id    varchar(255) primary key,
     balance      double precision not null,
     last_updated varchar(100)     not null
 );
---
+++ src/main/resources/db/migration/V2__prepareData.sql
@@ -0,0 +1,8 @@
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('1', 1000, '2019-05-19T11:21:56.605');
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('2', 1000, '2019-05-19T11:21:56.605');
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('3', 1000, '2019-05-19T11:21:56.605');
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('4', 1000, '2019-05-19T11:21:56.605');
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('5', 1000, '2019-05-19T11:21:56.605');
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('6', 1000, '2019-05-19T11:21:56.605');
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('7', 1000, '2019-05-19T11:21:56.605');
+INSERT INTO "public"."balances" ("client_id", "balance", "last_updated") VALUES ('8', 1000, '2019-05-19T11:21:56.605');
